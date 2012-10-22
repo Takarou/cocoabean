ec12d0e6ec6230eb5ed3ef150bb385f28285fe6d
Kristy Carter <carter_kristyle@knights.ucf.edu>
2012-10-22 13:51:29 -0400

    Added Keywords on the homepage, and added the javascript for Google Analytics

--- index.php
+++ index.php
@@ -5,6 +5,20 @@
 	<meta charset='utf-8'> 
 	
 	<link rel="stylesheet" href="css/style.css" media="screen" />
+    
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-35778705-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </head>
 <body id="Home">
 	
@@ -16,6 +30,14 @@
 			<p class="price">Now only $34.99!</p>
 			<p><img src="img/cheesecake002.jpg" alt="" /></p>
 		</div>		
+	</div>
+    
+    <div class="content">
+		<div class="container2">
+            <h2> A Little About Our Company </h2>
+            <p class="keys">Dive into your favorite gourmet treat, Chocolate!  At Cocoa Bean, we hand craft all of our recipes.  We make all of your chocolate dreams come true, all in one stop.<br /><br /> For our specialty chocolates, we start with the finest of ingredients.  We have whites, milks, and dark chocolates, all of which are from Belgium.  Some of which are solid pieces of decadence.  Others are filled with the finest of truffles.  All of which are delicious! <br /><br />The cupcakes and cakes are surely a treat.  They are all chocolate and range in flavors as well, including: white chocolate, traditional chocolate, German chocolate, and even red velvet (yes, it is a chocolate based cake… we just blew your mind, we know).  All of which are decadent and soft cakes.  Dried out dull cakes are a subject of the past with Cocoa Bean.<br /><br />Now onto our cheesecakes, which are transcendent!  First of all, who does not enjoy cheesecakes?  Well, ours are something to marvel at.  They include a variety of flavors with marbling of even more chocolate.  It’s like chocolate on chocolate on cheese action. So delicious! <br />Chocolate covered treats are a perfect treat for the certain someone special in your life.  This category includes the typical, but not typical in flavor, chocolate covered strawberries.  What a treat!  Other chocolate covered candies include more fruits like oranges and bananas (just in case you are worried about that girlish figure of yours).  <br /><br />Our chocolate drinks include, of course, hot chocolate.  But they are more than your typical hot chocolate that you get out of that gas station instant cocoa machine.  Ours are thick and perfect to snuggle up in front of a warm fire in the winter.  But we also offer iced chocolate drinks for those hotter summer months as well!<br /><br />   Surely you will find all of our categories of chocolate amazing.   We even make ourselves hungry talking about them all!</p>
+		<img src="img/shop.jpg" class="shop" alt="The Shop" />
+        </div>
 	</div>
 
 	<?php include 'php-includes/footer-include.php'; ?>
