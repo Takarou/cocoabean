efdd9c709de9057abd456fc638a852d54596d4e6
Juan Valera <jav094@gmail.com>
2012-10-24 11:45:15 -0400

    Added CRUD forms, made Google Analytics script external

--- contact.php
+++ contact.php
@@ -1,24 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Cocoa Bean: Gourmet Chocolate Treats</title>
+	<title>CocoaBean Contact</title>
 	<meta charset='utf-8'> 
 	
 	<link rel="stylesheet" href="css/style.css" media="screen" />
-    
-<script type="text/javascript">
 
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-35778705-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>    
+    <script type="text/javascript" src="js/google-analytics.js"></script>
 </head>
 <body id="Contact">
 	
