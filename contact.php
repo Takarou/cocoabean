82e773b01c590bec61260bace153c10d7b7ffa76
Kristy Carter <carter_kristyle@knights.ucf.edu>
2012-10-29 18:27:58 -0400

    responsive design implemented on contact page

--- contact.php
+++ contact.php
@@ -26,164 +26,198 @@
 	
 	<?php include 'php-includes/header-include.php'; ?>
 	
-	<div class="content">
-		<div class="container">
-			<h2>Company Bio</h2>
-            
-            <div class="peeps">
-			
-                <div class="container">				
-                    <dl>
-                        <div class="grouppreview">
-                            <dt><h3>David Parsons</h3><br />
-                            <p>Bio: <br /><br /> Stuff and things </p>
-                            </dt>
-                        	<dd class="part1">
-                                	<img class="grouppic" src="" alt="picture of David" />
-								</dd>
-                                <dd class="part2">
-                                	<ul>
-	                                	<li><h3> List of Responsibilities</h3></li><br />
-                                    	<li>Some of this</li>
-                                        <li>Some of that</li>
-                                        <li></li>
-                                    </ul>                                
-                                </dd>
-                                <dd class="part3">
-                                	<ul>
-	                                	<li><h3>How to Contact</h3></li><br />
-                                    	<li> <a href="#">email</a></li>
-                                    </ul> 
-                                </dd>
-                        </div>
-                        
-                        <div class="grouppreview">
-                            <dt><h3>Kristyle Carter</h3><br />
-                            <p>Bio: <br /><br />AKA Kristy is a web designer and enjoys making this look pretty.  Along with long walks on the beach.</p>
-                            </dt>
-                            	<dd class="part1">
-                                	<img class="grouppic" src="img/grouppics/KristyC.jpg" alt="picture of Kristy" />
-								</dd>
-                                <dd class="part2">
-                                	<ul>
-	                                	<li><h3> List of Responsibilities</h3></li><br />
-                                    	<li>Group Manager</li>
-                                        <li>Coordinating Tasks</li>
-                                        <li>Other Stuff (adding more later!) </li>
-                                    </ul>                                
-                                </dd>
-                                <dd class="part3">
-                                	<ul>
-	                                	<li><h3>How to Contact</h3></li><br />
-                                    	<li> <a href="#">email</a></li>
-                                    </ul> 
-                                </dd>
-                        </div>
-                        
-                        <div class="grouppreview">
-                            <dt><h3>Matt Kleinberg</h3><br />
-                            <p>Bio: <br /><br /> Stuff and things </p>
-                            </dt>
-                        	<dd class="part1">
-                                	<img class="grouppic" src="" alt="picture of Matt" />
-								</dd>
-                                <dd class="part2">
-                                	<ul>
-	                                	<li><h3> List of Responsibilities</h3></li><br />
-                                    	<li>Some of this</li>
-                                        <li>Some of that</li>
-                                        <li></li>
-                                    </ul>                                
-                                </dd>
-                                <dd class="part3">
-                                	<ul>
-	                                	<li><h3>How to Contact</h3></li><br />
-                                    	<li> <a href="#">email</a></li>
-                                    </ul> 
-                                </dd>
-                        </div>
-                        
-                        <div class="grouppreview">
-                            <dt><h3>Sherry McReally Long Last Name That Juan Cant Remember</h3><br />
-                            <p>Bio: <br /><br /> Stuff and things </p>
-                            </dt>
-                        	<dd class="part1">
-                                	<img class="grouppic" src="" alt="picture of Sherry" />
-								</dd>
-                                <dd class="part2">
-                                	<ul>
-	                                	<li><h3> List of Responsibilities</h3></li><br />
-                                    	<li>Some of this</li>
-                                        <li>Some of that</li>
-                                        <li></li>
-                                    </ul>                                
-                                </dd>
-                                <dd class="part3">
-                                	<ul>
-	                                	<li><h3>How to Contact</h3></li><br />
-                                    	<li> <a href="#">email</a></li>
-                                    </ul> 
-                                </dd>
-                        </div>
+    <div class="container">
+        <h2>Company Bio</h2>
 
-						<div class="grouppreview">
-							<dt><h3>Juan Valera</h3><br />
-                            <p>Bio: <br /><br />Juan Valera is a web and mobile developer with a background in user-centered design.</p>
-                            </dt>
-                        	<dd class="part1">
-                                	<img class="grouppic" src="" alt="picture of Juan" />
-								</dd>
-                                <dd class="part2">
-                                	<ul>
-	                                	<li><h3> List of Responsibilities</h3></li><br />
-                                    	<li>Styling</li>
-                                        <li>Frond End Development</li>
-                                        <li>Back End Development</li>
-                                        <li>A little of this a little of that</li>
-                                    </ul>                                
-                                </dd>
-                                <dd class="part3">
-                                	<ul>
-	                                	<li><h3>How to Contact</h3></li><br />
-                                    	<li> <a href="#">email</a></li>
-                                    </ul> 
-                                </dd>
-                                
-                        </div>
-                        
-                        <div class="grouppreview">
-							<dt><h3>Contact US</h3><br />
-
-                            </dt>
-                            
-                            <dd class="part1">
-                                <img class="grouppic" src="img/map.jpg" alt="map" />
-                            </dd>
-                            <dd class="part2">
-                                <ul>
-                                    <li><h3>Where we are located</h3></li><br />
-                                    <li>4000 Central Florida BLVD</li>
-                                    <li>Orlando, FL 32816</li>
-                                    <li>Visual Arts Building</li>
-                                </ul>  
-                            </dd> 
-                            <dd class="part3">
-                                	<ul>
-	                                	<li><h3>How to Contact Us</h3></li><br />
-                                    	<li> <a href="#">email</a></li><br />
-                                        <li>Call:555-555-5555</li>
-                                    </ul> 
-                                </dd>              
-    					</div> 
-										
-                    </dl>
+        <div class="grouppreview">
+            <div class="row">
+                <div class="threecol">
+                    <dt><h3>David Parsons</h3><br />
+                        <p>Bio: <br /><br /> Stuff and things </p>
+                    </dt>
                 </div>
-			
-                </div>   
+                
+                <div class="threecol">
+                    <img class="grouppic" src="" alt="picture of David" />
+                </div>
+                
+                <div class="threecol">
+                    <ul>
+                        <li><h3> List of Responsibilities</h3></li><br />
+                        <li>Some of this</li>
+                        <li>Some of that</li>
+                        <li></li>
+                    </ul>                                
+                </div>
+                
+                <div class="threecol last">
+                    <ul>
+                        <li><h3>How to Contact</h3></li><br />
+                        <li> <a href="#">email</a></li>
+                    </ul> 
+                <div><!--last-->
+                    
+            </div>
+        </div>
+        </div>
+    </div>
+    
+    <div class="grouppreview">
+        <div class="row">
+            <div class="threecol">
+                <dt><h3>Kristyle Carter</h3><br />
+                    <p>Bio: <br /><br />AKA Kristy is a web designer and enjoys making this look pretty.  Along with long walks on the beach.</p>
+                </dt>
+            </div>
+                        
+            <div class="threecol">
+                <img class="grouppic" src="img/grouppics/KristyC.jpg" alt="picture of Kristy" />
+            </div>
+                        
+            <div class="threecol">
+                <ul>
+                    <li><h3> List of Responsibilities</h3></li><br />
+                    <li>Group Manager</li>
+                    <li>Coordinating Tasks</li>
+                    <li>Other Stuff (adding more later!) </li>
+                </ul> 
+            </div>                               
+                            
+            <div class="threecol last">    
+                <ul>
+                    <li><h3>How to Contact</h3></li><br />
+                    <li> <a href="#">email</a></li>
+                </ul> 
+            </div>
+        </div>
+    </div>
+    
+    <div class="grouppreview">
+        <div class="row">
+            <div class="threecol">                
+                <dt><h3>Matt Kleinberg</h3><br />
+                    <p>Bio: <br /><br /> Stuff and things </p>
+                </dt>
+            </div>
             
-			</div>
-		</div>
-	</div><!--/.content-->
+            <div class="threecol">
+                <img class="grouppic" src="" alt="picture of Matt" />
+            </div>
+            
+            <div class="threecol">
+                <ul>
+                    <li><h3> List of Responsibilities</h3></li><br />
+                    <li>Some of this</li>
+                    <li>Some of that</li>
+                    <li></li>
+                </ul>                                
+            </div>
+            
+            <div class="threecol last">
+                <ul>
+                    <li><h3>How to Contact</h3></li><br />
+                    <li> <a href="#">email</a></li>
+                </ul>
+            </div> 
+        </div>
+    </div>
+    
+    <div class="grouppreview">
+        <div class="row">
+            <div class="threecol">
+                <dt><h3>Sherry McReally Long Last Name That Juan Cant Remember</h3><br />
+                <p>Bio: <br /><br /> Stuff and things </p>
+                </dt>
+            </div>
+            
+            <div class="threecol">
+                <img class="grouppic" src="" alt="picture of Sherry" />
+            </div>
+            
+            <div class="threecol">
+                <ul>
+                    <li><h3> List of Responsibilities</h3></li><br />
+                    <li>Some of this</li>
+                    <li>Some of that</li>
+                    <li></li>
+                </ul>                                
+            </div>
+            
+            <div class="threecol last">
+                <ul>
+                    <li><h3>How to Contact</h3></li><br />
+                    <li> <a href="#">email</a></li>
+                </ul> 
+            </div>
+            
+        </div>
+	</div>    
+   
+	<div class="grouppreview">
+        <div class="row">
+            <div class="threecol">            
+                <dt><h3>Juan Valera</h3><br />
+                <p>Bio: <br /><br />Juan Valera is a web and mobile developer with a background in user-centered design.</p>
+                </dt>
+            </div>
+            
+            <div class="threecol">
+                  <img class="grouppic" src="" alt="picture of Juan" />
+            </div>
+            
+            <div class="threecol">
+                <ul>
+                    <li><h3> List of Responsibilities</h3></li><br />
+                    <li>Styling</li>
+                    <li>Frond End Development</li>
+                    <li>Back End Development</li>
+                    <li>A little of this a little of that</li>
+                </ul>
+            </div>
+            
+            <div class="threecol last">
+                <ul>
+                    <li><h3>How to Contact</h3></li><br />
+                    <li> <a href="#">email</a></li>
+                </ul> 
+            </div>
+                    
+        </div>
+    </div>
+    
+    <div class="grouppreview">
+        <div class="row">
+        
+            <div class="threecol">
+                <dt><h3>Contact US</h3><br />
+                </dt>
+            </div>
+            
+            <div class="threecol">
+                <img class="grouppic" src="img/map.jpg" alt="map" />
+            </div>
+            
+            <div class="threecol">
+                <ul>
+                    <li><h3>Where we are located</h3></li><br />
+                    <li>4000 Central Florida BLVD</li>
+                    <li>Orlando, FL 32816</li>
+                    <li>Visual Arts Building</li>
+                </ul>  
+            </div>
+            
+            <div class="threecol last">
+                <ul>
+                    <li><h3>How to Contact Us</h3></li><br />
+                    <li> <a href="#">email</a></li><br />
+                    <li>Call:555-555-5555</li>
+                </ul>             
+            </div> 
+                                    
+        </div>
+    </div>
+
+</div><!--/.content-->
 
 	<?php include 'php-includes/footer-include.php'; ?>
 	
