766d0972edc8ae4da4197b32a0368243667c6b9d
Kristy Carter <carter_kristyle@knights.ucf.edu>
2012-10-29 16:53:20 -0400

    Added the css and javascript files to all the pages Also added the grid to home page thus far

--- index.php
+++ index.php
@@ -6,27 +6,48 @@
 	
 	<link rel="stylesheet" href="css/style.css" media="screen" />
     
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	
+	<!-- 1140px Grid styles for IE -->
+	<!--[if lte IE 9]><link rel="stylesheet" href="css/ie.css" type="text/css" media="screen" /><![endif]-->
+
+	<!-- The 1140px Grid - http://cssgrid.net/ -->
+	<link rel="stylesheet" href="css/1140.css" type="text/css" media="screen" />
+	
+	<!-- Your styles -->
+	<link rel="stylesheet" href="css/styles.css" type="text/css" media="screen" />
+	
+	<!--css3-mediaqueries-js - http://code.google.com/p/css3-mediaqueries-js/ - Enables media queries in some unsupported browsers-->
+	<script type="text/javascript" src="js/css3-mediaqueries.js"></script>
+    
 	<script type="text/javascript" src="js/google-analytics.js"></script>
 </head>
 <body id="home">
 	
 	<?php include 'php-includes/header-include.php'; ?>
 	
-	<div class="content">
-		<div class="featured">
-			<h2><a href="item.php?id=2">Chocolate Chip Cookie Dough Cheesecake</a></h2>
-			<p class="price">Now only $34.99!</p>
-			<p><img src="img/cheesecake002.jpg" alt="" /></p>
-		</div>		
-	</div>
-    
-    <div class="content">
-		<div class="container2">
-            <h2> A Little About Our Company </h2>
-            <p class="keys">Dive into your favorite gourmet treat, Chocolate!  At Cocoa Bean, we hand craft all of our recipes.  We make all of your chocolate dreams come true, all in one stop.<br /><br /> For our specialty chocolates, we start with the finest of ingredients.  We have whites, milks, and dark chocolates, all of which are from Belgium.  Some of which are solid pieces of decadence.  Others are filled with the finest of truffles.  All of which are delicious! <br /><br />The cupcakes and cakes are surely a treat.  They are all chocolate and range in flavors as well, including: white chocolate, traditional chocolate, German chocolate, and even red velvet (yes, it is a chocolate based cake… we just blew your mind, we know).  All of which are decadent and soft cakes.  Dried out dull cakes are a subject of the past with Cocoa Bean.<br /><br />Now onto our cheesecakes, which are transcendent!  First of all, who does not enjoy cheesecakes?  Well, ours are something to marvel at.  They include a variety of flavors with marbling of even more chocolate.  It’s like chocolate on chocolate on cheese action. So delicious! <br />Chocolate covered treats are a perfect treat for the certain someone special in your life.  This category includes the typical, but not typical in flavor, chocolate covered strawberries.  What a treat!  Other chocolate covered candies include more fruits like oranges and bananas (just in case you are worried about that girlish figure of yours).  <br /><br />Our chocolate drinks include, of course, hot chocolate.  But they are more than your typical hot chocolate that you get out of that gas station instant cocoa machine.  Ours are thick and perfect to snuggle up in front of a warm fire in the winter.  But we also offer iced chocolate drinks for those hotter summer months as well!<br /><br />   Surely you will find all of our categories of chocolate amazing.   We even make ourselves hungry talking about them all!</p>
-		<img src="img/shop.jpg" class="shop" alt="The Shop" />
+	<div class="container">
+    	<div class="row">
+        	<div class="twelvecol last">
+                <h2><a href="item.php?id=2">Chocolate Chip Cookie Dough Cheesecake</a><br /><span class="price">Now Only $34.99!</span></h2>
+                <img class="featured" src="img/cheesecake002.jpg" alt="" />
+			</div>		
+		</div>
+        
+        <div class="row">
+        	<div class="twelvecol last">    
+        		<h2> A Little About Our Company </h2>
+            </div>
         </div>
-	</div>
+        
+        <div class="row">
+        	<div class="sixcol">
+       			<p>Dive into your favorite gourmet treat, Chocolate!  At Cocoa Bean, we hand craft all of our recipes.  We make all of your chocolate dreams come true, all in one stop.<br /> For our specialty chocolates, we start with the finest of ingredients.  We have whites, milks, and dark chocolates, all of which are from Belgium.  Some of which are solid pieces of decadence.  Others are filled with the finest of truffles.  All of which are delicious! <br />The cupcakes and cakes are surely a treat.  They are all chocolate and range in flavors as well, including: white chocolate, traditional chocolate, German chocolate, and even red velvet (yes, it is a chocolate based cake… we just blew your mind, we know).  All of which are decadent and soft cakes.  Dried out dull cakes are a subject of the past with Cocoa Bean.<br />Now onto our cheesecakes, which are transcendent!  First of all, who does not enjoy cheesecakes?  Well, ours are something to marvel at.  They include a variety of flavors with marbling of even more chocolate.  It’s like chocolate on chocolate on cheese action. So delicious! <br />Chocolate covered treats are a perfect treat for the certain someone special in your life.  This category includes the typical, but not typical in flavor, chocolate covered strawberries.  What a treat!  Other chocolate covered candies include more fruits like oranges and bananas (just in case you are worried about that girlish figure of yours).  <br />Our chocolate drinks include, of course, hot chocolate.  But they are more than your typical hot chocolate that you get out of that gas station instant cocoa machine.  Ours are thick and perfect to snuggle up in front of a warm fire in the winter.  But we also offer iced chocolate drinks for those hotter summer months as well!<br />Surely you will find all of our categories of chocolate amazing.   We even make ourselves hungry talking about them all!</p>
+			</div>
+            <div class="sixcol last">
+            	<img src="img/shop.jpg" class="shop" alt="The Shop" />
+            </div>
+    </div>
 
 	<?php include 'php-includes/footer-include.php'; ?>
 	
