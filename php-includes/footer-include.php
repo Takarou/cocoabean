fc48069b44dec4d02cdafb6ebb82105f631a95fc
Kristy Carter <carter_kristyle@knights.ucf.edu>
2012-11-02 10:33:32 -0400

    adding header divs and styling

--- php-includes/footer-include.php
+++ php-includes/footer-include.php
@@ -1,5 +1,16 @@
 <footer>
-	<p>&copy; Cocoa Bean<sup>TM</sup> Inc. All Rights Reserved.</p>
-	<p>This site is not official and is an assignment for a UCF Digital Media course.</p>
-	<p>Designed by <a href="contact.php">Team Cocoabean</a>.</p>
+	<div class="row">
+    	<div class="twelve last">
+        	<div class="line"></div>
+        </div>
+   	</div>		
+	<div class="row">
+    	<div class="threecol"></div>
+    	<div class="sixcol">
+            <p>&copy; Cocoa Bean<sup>TM</sup> Inc. All Rights Reserved.</p>
+            <p>This site is not official and is an assignment for a UCF Digital Media course.</p>
+            <p>Designed by <a href="contact.php">Team Cocoabean</a>.</p>
+        </div>
+        <div class="threecol last"></div>
+    </div>
 </footer>
--- php-includes/header-include.php
+++ php-includes/header-include.php
@@ -1,37 +1,78 @@
 <header>
-	<div id="logo">
-		<h1><a href="index.php">Cocoa Bean</a></h1>
+	<div class="row">
+    	<div class="logo">
+            <div class="twocol last">
+                <img src="img/logo.png" alt="logo" />
+            </div>
+        </div>
+ 
+    	<div class="moved">   
+            
+            <div class="threecol">
+                <h1 class="logo"><a href="index.php">Cocoa Bean</a></h1>
+            </div>
+    
+            <div class="check">
+            <div class="threecol">
+            	
+                    <form method="post" action="search.php" id="searchform"> 
+                        <input type="search" id="searchbox" /> 
+                        <input type="submit" id="search" value="Search" />
+                    </form>
+                </div>
+            </div>
+            
+            
+            <div class="cart">
+                    
+                <form action="login.php" method="post">
+                    <div class="onecol">
+                    <input id="login-username" placeholder="Username" type="text"></input>	
+                    </div>
+                    <div class="onecol">
+                    <input id="login-password" placeholder="Password" type="text"></input>
+                    </div>
+                    <div class="onecol">
+                    <input id="login-submit" type="submit" value="Submit"></input>
+                    </div>
+                    <div class="onecol last">
+                    <a id="login-signup" href="#">Sign up</a>
+                    </div>
+                </form>
+			</div>
+        </div>
 	</div>
-	
-	<form method="post" action="search.php" id="searchform"> 
-		<input type="search" id="searchbox" /> 
-		<input type="submit" id="search" value="Search" />
-	</form>
-
-	<div class="cart">		
-		<form action="login.php" method="post">
-			<input id="login-username" placeholder="Username" type="text"></input>	
-			<input id="login-password" placeholder="Password" type="text"></input>
-			<input id="login-submit" type="submit" value="Submit"></input>
-			<a id="login-signup" href="#">Sign up</a>
-		</form>
-		
-		<div id="cart-contents">			
-			<a href="cart.php">In your cart: 3 items</a><br/>			
-			<a href="cart.php" id="cart-checkout">Checkout</a><br/>
-		</div>		
-		<div id="cart-image"></div>
-	</div><!--/.cart-->
-		
+        		
+    
+    <div class="row">
 	<nav>
 		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="products.php?category=Cakes">Cakes</a></li>
-			<li><a href="products.php?category=Cheesecake">Cheesecake</a></li>
-			<li><a href="products.php?category=Druit">Drinks</a></li>
-			<li><a href="products.php?category=Fruit">Fruit</a></li>
-			<li><a href="products.php?category=Specialty">Specialty</a></li>
-			<li><a href="contact.php">Contact Us</a></li>
+			<div class="onecol"><li><a href="index.php">Home</a></li></div>
+			<div class="onecol"><li><a href="products.php?category=Cakes">Cakes</a></li></div>
+			<div class="onecol"><li><a href="products.php?category=Cheesecake">Cheesecake</a></li></div>
+			<div class="onecol"><li><a href="products.php?category=Druit">Drinks</a></li></div>
+			<div class="onecol"><li><a href="products.php?category=Fruit">Fruit</a></li></div>
+			<div class="onecol"><li><a href="products.php?category=Specialty">Specialty</a></li></div>
+			<div class="onecol"><li><a href="contact.php">Contact</a></li></div>
 		</ul>	
-	</nav>				
+
+        <div id="cart-contents">
+        	<div class="onecol"></div> 			
+	    	<div class="twocol">
+                <a href="cart.php">In your cart: 3 items</a><br/>			
+			</div>
+            <div class="onecol">
+                <a href="cart.php" id="cart-checkout">Checkout</a><br/>
+            </div>
+        </div>	
+        <div class="onecol last">
+			<img src="img/cart.png" alt="cart" />
+		</div>
+    </nav>
+    
+    <div class="row">
+    	<div class="twelve last">
+        	<div class="line"></div>
+        </div>
+   	</div>				
 </header>
