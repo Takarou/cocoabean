f29f438d26f93f8c635bea3e5c55b12f37f33560
Sherry <sherry@knights.ucf.edu>
2012-11-04 23:45:02 -0500

    Added Review and light box stuff and bio.  Reviews needs to be thoroughly tested and styled (see bottom of style.css)

---
+++ local-database.php
@@ -0,0 +1,4 @@
+<?php
+	$connection = mysql_connect("localhost", "sherry", "") or die(mysql_error());
+	$db = mysql_select_db("ccb") or die(mysql_error());
+?>
--- products.php
+++ products.php
@@ -15,12 +15,15 @@
 		@import url("css/reset.css") screen;
 		@import url("css/1140.css") screen;
 		@import url("css/style.css") screen;
+		@import url("css/jquery.fancybox.css") screen;
 	</style>
 		
 	<!--css3-mediaqueries-js - http://code.google.com/p/css3-mediaqueries-js/ - Enables media queries in some unsupported browsers-->
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 	<script type="text/javascript" src="js/cart.js"></script>
 	<script type="text/javascript" src="js/css3-mediaqueries.js"></script>
+	<script type="text/javascript" src="js/jquery.fancybox.js"></script>
+	<script type="text/javascript" src="js/products.js"></script>
     
 	<script type="text/javascript" src="js/google-analytics.js"></script>
 </head>
@@ -49,18 +52,61 @@
 																														
 				// While there are more results in the result set, loop through them and get select info out.
 				while ($results = mysql_fetch_array($query))
-				{					
+				{	
 					echo"
 					<div class='threecol'>
-		                <div class='product'>		                	
-		                	<img src='".$results['productImg']."' alt='".$results['productName']."'></img>
-		                	<h3>".$results['productName']."</h3>
+		                <div class='product num-".$results['id']."'>		                	
+		                	<a href='#lBox-".$results['id']."' class='item'><img src='".$results['productImg']."' alt='".$results['productName']."'></img></a>
+		                	<h3><a href='#lBox-".$results['id']."' class='item'>".$results['productName']."</a></h3>
 		                	<p>$".$results['price']."</p>
 		                	<a class='buy-button' id='id_".$results['id']."'>Add to Cart</a>
 		                    <p>".$results['sDesc']."</p>
-		                    <p> Rating: ".$results['rating']."</p>
+		                    <p> Rating: <span class='rating-".$results['rating']."'>".$results['rating']."</span></p>
 		                </div>
-	            	</div>";					
+	            	</div>
+					";
+					
+					// Query database for this product's reviews
+					$curr_id = $results['id'];
+					$searchReviews = mysql_query("SELECT * FROM reviews WHERE prod_id='$curr_id'") or die(mysql_error());
+					
+					// Write lightbox div with reviews for each product
+					echo '
+						<div style="display: none;">
+						<div class="lBox" id="lBox-'.$results['id'].'">
+							<h3>'.$results['productName'].'</h3>
+							<p class="price">$'.$results['price'].'</p>
+							<img src="'.$results['productImg'].'" alt="" />
+							<p>'.$results['lDesc'].'</p>
+							<p><a class="buy-button" id="id_'.$results['id'].'">Add to Cart</a></p>
+							
+							<h4>Reviews</h4>';
+							
+							// If the rating isn't 0, then we know reviews have been left (minimum rating is 1)
+							if ($results['rating'] != 0)	{
+								while($reviews = mysql_fetch_array($searchReviews))	{
+									
+									echo '
+										<div class="review">
+											<p class="rating-'.$reviews['rating'].'">'.$reviews['rating'].'</p>
+											<p>'.$reviews['reviewtext'].'</p>
+											<p>by '.$reviews['user_id'].'</p>
+										</div>
+									';
+								
+								} //end while
+							} //end if
+							
+							// If there are no reviews for this product:
+							else	{
+								echo '<p>Be the first to review this product.</p>';
+							}
+							
+							include 'review.php';
+							
+					echo '		</div>
+						</div>
+					';
 				}				
 		    ?>
 	       
@@ -97,7 +143,7 @@
             </div>  -->
            
         </div><!--/.row-->	   
->>>>>>> be1af799b9cc348b1378a207647f85f67dd9fd8b
+<!-- >>>>>>> be1af799b9cc348b1378a207647f85f67dd9fd8b -->
 
     </div><!--/.container-->
 
