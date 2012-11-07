49dc7b0dfefe91c62b3553a2755a168a795662ec
Juan Valera <jav094@gmail.com>
2012-11-07 18:25:13 -0500

    Adding 'remove from cart' button

---
+++ removeCart.php
@@ -0,0 +1,34 @@
+<?php
+	session_start();
+	include 'database.php';
+	
+	$testId = $_POST['id'];
+	$item = array(
+		'name' => '',
+		'qty' => 1,
+		'img' => '',
+		'price' => ''
+	);
+	$testQuery = mysql_query("SELECT * FROM product WHERE id='$testId'");
+	while($results = mysql_fetch_array($testQuery)){
+		if(array_key_exists($results['id'], $_SESSION['cart'])){
+			$_SESSION['cart'][$results['id']]['qty'] += 1;
+			$_SESSION['cart'][$results['id']]['price'] = $results['price'] * $_SESSION['cart'][$results['id']]['qty'];
+		}else{
+			$item['name'] = $results['productName'];
+			$item['price'] = $results['price'];
+			$item['img'] = $results['productImg'];
+		
+			$_SESSION['cart'][$results['id']] = $item;
+		}
+	}
+	if(isset($_SESSION['user'])){
+		$user = $_SESSION['user'];
+		$saveCart = json_encode($_SESSION['cart']);
+		mysql_query("UPDATE users SET cart='$saveCart' WHERE username='$user'");
+		echo $saveCart;
+		
+	}else{
+		json_encode($_SESSION['cart']);
+	}
+?>
